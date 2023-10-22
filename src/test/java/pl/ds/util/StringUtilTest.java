912239cef009dc96aac7ce33bdb501fa94c6ca10
ppszczepaniak <p.p.szczepaniak@gmail.com>
2023-10-22 18:16:41 +0200

    add more test cases

--- src/test/java/pl/ds/util/StringUtilTest.java
+++ src/test/java/pl/ds/util/StringUtilTest.java
@@ -25,4 +25,70 @@ class StringUtilTest {
         boolean result = StringUtil.contains(string, pattern);
         assertEquals(expected, result);
     }
+
+    @ParameterizedTest
+    @CsvSource(value = {
+            "adcab, ab, true",
+            "abcA, A, true",
+            "' ', ' ', true",
+            "' ', '  ', false",
+            "'  ', '  ', true",
+            "'  ', ' ', true",
+            "'', a, false",
+            "' abc', ' a', true",
+            "' a bc', ' a', true",
+            "'         a b c', ' ', true",
+            "'abc ', 'c ', true",
+            "'abc ', ' ', true",
+            "'abc ', '  ', false",
+            "a, a, true",
+            "a, b, false",
+            "a, ab, false",
+            "b, ab, false",
+            "ab, ab, true",
+            "aba, aa, false",
+            "axaxaxa, aa, false",
+            "axaxaaxa, aa, true",
+            "axaxaxaa, aa, true",
+            "axaxaxaaaaa, aa, true",
+            "axaxaxaxaxaaxaaxaaxaxa, aa, true",
+            "ba, cba, false",
+            "abcA, abc, true",
+            "abcd, abcd, true",
+            "abcabcabcabcd, abcd, true",
+            "abcabcabcabcd, ca, true",
+            "aaaaaaaaab, ab, true",
+            "bbbbbb, ab, false",
+            "aba, abc, false",
+            "abc, abc, true",
+            "babc, abc, true",
+            "ababc, abc, true",
+            "aabc, abc, true",
+            "axabc, abc, true",
+            "bababc, abc, true",
+            "abababc, abc, true",
+            "bbbababababc, abc, true",
+            "abcd, abcde, false",
+            "xyz, ab, false",
+    }, nullValues = {"NULL"})
+    void regularTests(String string, String pattern, boolean expected) {
+        boolean result = StringUtil.contains(string, pattern);
+        assertEquals(expected, result);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {
+            "NULL, abc, false",
+            "abc, null, false",
+            "abc, NULL, false",
+            "NULL, null, false",
+            "null, NULL, false",
+            "null, null, true",
+            "null123, null, true",
+            "123null123, null, true",
+    }, nullValues = {"NULL"})
+    void nullTests(String string, String pattern, boolean expected) {
+        boolean result = StringUtil.contains(string, pattern);
+        assertEquals(expected, result);
+    }
 }
