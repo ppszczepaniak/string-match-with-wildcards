177cb5e47748b9ec93e3b7cd745e1cf08a1564ea
ppszczepaniak <p.p.szczepaniak@gmail.com>
2023-10-22 18:13:52 +0200

    bump JUnit version + add parametrized tests

--- src/test/java/pl/ds/util/StringUtilTest.java
+++ src/test/java/pl/ds/util/StringUtilTest.java
@@ -1,14 +1,28 @@
 package pl.ds.util;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class StringUtilTest {
+class StringUtilTest {
 
-    @Test
-    void testIsSubstring() {
-        assertEquals(true, StringUtil.contains("abcd", "bc"));
+    @ParameterizedTest
+    @CsvSource(value = {
+            "NULL, *, false",
+            "*, NULL, false",
+            "'', '', true",
+            "abc, '', true",
+            "abc, a, true",
+            "abc, a*, true",
+            "a*c, a\\*, true",
+            "a*c, abc, false",
+            "abc, A, false",
+            "abc, abcd, false",
+            "abcd, bc, true",
+    }, nullValues = {"NULL"})
+    void originalTests(String string, String pattern, boolean expected) {
+        boolean result = StringUtil.contains(string, pattern);
+        assertEquals(expected, result);
     }
-
 }
